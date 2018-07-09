aa20a98c82895649eeb109adf3f468df9bf309f6
Arron Gibson <github@infinitymedia.ca>
2018-07-09 16:04:20 -0400

    Adding creds to seperate file for SOC

---
+++ src/base-creds.js
@@ -0,0 +1,8 @@
+
+const creds = {
+	apiKey: "AIzaSyD7x5KGwOnZs1eOjHzR_tJb3YbSs1tF0jo",
+	authDomain: "react-perilous-trivia.firebaseapp.com",
+	databaseURL: "https://react-perilous-trivia.firebaseio.com",
+};
+
+export default creds;
